1408bb11dba75d7bba6e6a2e225f6701afbb58e9
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-05-29 23:09:51 +0530

    created alternate-flavour

---
+++ 6-alternate-flavour.js
@@ -0,0 +1,9 @@
+//One way of setting exports
+module.exports.items = ['item1','item2']
+
+//Another way of setting exports
+const person = {
+    name: 'nirmal',
+}
+
+module.exports.singlePerson = person
