edeaf7eead58f76a699f3f981b64e8518598b72b
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-05-31 10:50:58 +0530

    updated gitignore

--- app.js
+++ app.js
@@ -13,3 +13,9 @@
 // ways to create - manual or automatic 
 // npm init creates it automatically 
 // npm init -y uses default options 
+
+const lodash = require('lodash')
+
+const items = [1,[2,[3,[4]]]]
+const newItems = lodash.flattenDeep(items);
+console.log(newItems);
