b5d14c908e6beb80c3d996755c8b16f71273347d
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-06-09 11:32:38 +0530

    created readstream

--- app.js
+++
@@ -1,9 +0,0 @@
-const {createReadStream} = require('fs')
-
-const stream = createReadStream('./content/big.txt')
-
-stream.on('data',(result)=>{
-    console.log(result);
-})
-
-// Reads data in 64 kilobyte chunks
